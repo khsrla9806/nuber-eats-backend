1e9c28724da92f0d755525cd2af17d12b9cf89f9
khsrla9806 <kimhunsope@naver.com>
2023-07-22 20:51:36 +0900

    feat: Repository Imports 하기
    - TypeOrmNodule.forFeature([Restaurant])를 사용하여 Repository 등록
    - forFeature()에 Entity가 여러개이면 배열의 값으로 추가해서 넣을 수 있다.

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RestaurantResolver } from './restaurants.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurant.entity';
 
 @Module({
     providers:[RestaurantResolver],
+    imports: [TypeOrmModule.forFeature([Restaurant])], // forFeature()을 통해서 Repository 등록
 })
 export class RestaurantsModule {}
