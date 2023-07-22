bd53beb7c1acfe26ff362f69851adb4862cd2a0b
khsrla9806 <kimhunsope@naver.com>
2023-07-22 11:35:14 +0900

    feat: TypeORM 적용 및 env-cross와 config를 이용한 환경 변수 설정
    - @nestjs/typeorm 설치
    - pg 설치 (postgres)
    - cross-env 설치
    - @nestjs/config 설치
    - start:dev와 start 명령어에 cross-env NODE_ENV 추가
    - app.module.ts에 TypeOrmModule과 ConfigModule 추가 (envFilePath, ignoreEnvFile 설정)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 우리 애플레케이션 어디서든 ConfigModule에 접근이 가능하도록 설정
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod', // prod 상태에서는 env 파일을 사용해서 하지 않음
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT, // +를 붙여서 string -> number 변경
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true, // DB와 Nest 모듈의 동기화
+      logging: true, // 콘솔에 데이터베이스 관련 부분을 찍음
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
