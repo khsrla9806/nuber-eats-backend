d2f6ddb503dd5213edb05ef5ea776ceb84181609
khsrla9806 <kimhunsope@naver.com>
2023-07-22 22:52:22 +0900

    feat: RestaurantResolver에서 정의한 updateDto와 updateRestaurant를 적용
    - 예외가 발생한 경우에는 update 쿼리를 실행하지 않고, false를 반환
    - 정상적인 흐름인 경우에는 update 쿼리를 실행하고, true를 반환

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { Restaurant } from "./entities/restaurant.entity";
 import { CreateRestaurantDto } from "./dtos/create-restaurant.dto";
 import { RestaurantService } from "./restaurants.service";
+import { UpdateRestaurantDto } from "./dtos/update-restaurant.dto";
 
 
 
@@ -16,9 +17,20 @@ export class RestaurantResolver {
     }
 
     @Mutation(returns => Boolean)
-    async createRestaurant(@Args("input") createRestaurantDto: CreateRestaurantDto): Promise<boolean> {
+    async createRestaurant(@Args('input') createRestaurantDto: CreateRestaurantDto): Promise<boolean> {
         try {
             await this.restaurantService.createRestaurant(createRestaurantDto);
+            return true;
+        } catch(e) {
+            console.log(e);
+            return false;
+        }
+    }
+
+    @Mutation(returns => Boolean)
+    async updateRestaurant(@Args('input') updateRestaurantDto: UpdateRestaurantDto): Promise<boolean> {
+        try {
+            await this.restaurantService.updateRestaurant(updateRestaurantDto);
             return true;
         } catch(e) {
             console.log(e);
