347cc7c814ef5371789533ab5361164e10a076d8
khsrla9806 <kimhunsope@naver.com>
2023-07-22 22:51:26 +0900

    feat: RestaurantService에 updateRestaurant 메서드 구현
    - Repository의 update()는 id에 해당하는 Entity가 존재하지 않아도 update 쿼리를 날린다.
    - 그래서 findOneBy()를 사용해서 해당되는 Entity가 존재하는지 확인한 후 없으면 BadRequestException 발생
    - 존재한다면 update 쿼리를 날려 Entity를 수정한다.

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Restaurant } from "./entities/restaurant.entity";
 import { Repository } from "typeorm";
 import { CreateRestaurantDto } from "./dtos/create-restaurant.dto";
+import { UpdateRestaurantDto } from "./dtos/update-restaurant.dto";
+import { NotFoundError } from "rxjs";
 
 @Injectable()
 export class RestaurantService {
@@ -17,5 +19,18 @@ export class RestaurantService {
         const restaurant = this.restaurantRepository.create(createRestaurantDto);
 
         return this.restaurantRepository.save(restaurant);
+    }
+
+    async updateRestaurant({id, data}: UpdateRestaurantDto) {
+        /* id로 Restaurant를 찾아서 없으면 BadRequestException을 던짐 */
+        const foundedRestaurant = await this.restaurantRepository.findOneBy({id});
+        if (!foundedRestaurant) {
+            throw new BadRequestException(id + '에 해당하는 식당이 존재하지 않습니다.');
+        }
+        /*
+            id로 해당하는 Entity를 찾고, 수정 사항이 있는 data를 적용한다.
+            여기서 주의해야할 부분은 update는 id에 해당하는 Entity가 없어도 에러를 발생시키지 않는다.
+        */
+        this.restaurantRepository.update(id, {...data});
     }
 }
