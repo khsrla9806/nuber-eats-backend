e56f0efd24c4565b07e23f03078416b8fabdb6bc
khsrla9806 <kimhunsope@naver.com>
2023-07-22 20:55:24 +0900

    feat: module에 providers에 Service 등록 후 resolver에 의존성 주입
    - module의 Providers에 RestaurantService 추가
    - 생성자를 통해서 RestaurantService 의존성 주입
    - 기존 restaurants 쿼리 메서드에 Service에서 정의한 getAll() 메서드 적용

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RestaurantResolver } from './restaurants.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
 
 @Module({
-    providers:[RestaurantResolver],
     imports: [TypeOrmModule.forFeature([Restaurant])], // forFeature()을 통해서 Repository 등록
+    providers: [RestaurantResolver, RestaurantService],
 })
 export class RestaurantsModule {}
