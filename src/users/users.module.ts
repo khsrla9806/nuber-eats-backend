4ad9c94591bb2d46fdfe0f54085edf5d2ac9265f
khsrla9806 <kimhunsope@naver.com>
2023-07-23 14:49:16 +0900

    feat: User Repository, Service, Resolver 생성 후 의존관계 형성
    - Repository는 module에서 forFeature()를 통해 가져옴
    - Service는 파일 생성 후 생성자를 통해 Repository 의존성 주입
    - Resolver는 파일 생성 후 생성자를 통해 Service 의존성 주입

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UserService } from './users.service';
+import { UserResolver } from './users.resolver';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([User])], // Repository 가져오기
+    providers: [UserService, UserResolver]
+})
 export class UsersModule {}
---
+++ src/users/users.resolver.ts
@@ -0,0 +1,9 @@
+import { Resolver } from "@nestjs/graphql";
+import { User } from "./entities/user.entity";
+import { UserService } from "./users.service";
+
+@Resolver(of => User)
+export class UserResolver {
+    constructor(private readonly userService: UserService) {}
+    
+}
