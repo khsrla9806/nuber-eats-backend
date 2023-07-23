d24942cc1f701be80d841be3b8fbb447b89866a8
khsrla9806 <kimhunsope@naver.com>
2023-07-23 16:36:58 +0900

    refactor: User Entity의 UserRole을 Enum 타입으로 수정 후 TypeORM에도 Enum 등록
    - TypeORM에 등록할 때는 registerEnumType()을 사용
    - TypeORM 적용 후 @Field()에 type을 UserRole로 변경
    - Entity @Column에도 type을 'enum'으로 하고, enum에 UserRole 설정
    - 이렇게 하면 DB에는 1, 2, 3 이런식으로 값이 할당되는데, 이 부분은 Enum의 Ordinary로 저장
    - 나중에 다른 방법 찾아보자

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,9 +1,17 @@
 import { Field, InputType, ObjectType } from "@nestjs/graphql";
 import { type } from "os";
+import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
 import { CoreEntity } from "src/common/entities/core.entity";
 import { Column, Entity } from "typeorm";
 
-type UserRole = 'client' | 'owner' | 'delivery';
+enum UserRole {
+    Client,
+    Owner,
+    Delivery
+}
+
+/* GrahpQL에도 Enum을 등록할 수 있다. */
+registerEnumType(UserRole, { name: 'UserRole' });
 
 @InputType({ isAbstract: true })
 @ObjectType()
@@ -18,7 +26,7 @@ export class User extends CoreEntity {
     @Column()
     password: string;
 
-    @Field(type => String)
-    @Column()
+    @Field(type => UserRole) // GraphQL에 등록한 enum을 @Field의 type으로 지정
+    @Column({type: 'enum', enum: UserRole})
     role: UserRole;
 }
