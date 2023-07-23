97c10ba950b2f84e6b176ded7276f39cda8b1043
khsrla9806 <kimhunsope@naver.com>
2023-07-23 16:41:23 +0900

    feat: bcrypt 라이브러리와 @BeforeInsert Listener를 사용하여 비밀번호 해싱
    - npm i bcrypt
    - npm i @types/bcrypt --dev-only
    - @BeforeInsert는 Entity가 DB에 Insert 되기 전에 실행하는 Listener

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,8 @@
-import { Field, InputType, ObjectType } from "@nestjs/graphql";
-import { type } from "os";
 import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
 import { CoreEntity } from "src/common/entities/core.entity";
-import { Column, Entity } from "typeorm";
+import { BeforeInsert, Column, Entity } from "typeorm";
+import * as bcrypt from "bcrypt";
+import { InternalServerErrorException } from "@nestjs/common";
 
 enum UserRole {
     Client,
@@ -29,4 +29,15 @@ export class User extends CoreEntity {
     @Field(type => UserRole) // GraphQL에 등록한 enum을 @Field의 type으로 지정
     @Column({type: 'enum', enum: UserRole})
     role: UserRole;
+
+    /* DB에 Insert 되기 전에 비밀번호를 해싱해서 넣는다. (round = 10, 총 10번 진행해라) */
+    @BeforeInsert()
+    async hashPassword(): Promise<void> {
+        try {
+            this.password = await bcrypt.hash(this.password, 10); // 두 번째 인자가 round 값 (bcrypt 추천 값)
+        } catch (e) {
+            console.log(e);
+            throw new InternalServerErrorException();
+        }
+    }
 }
