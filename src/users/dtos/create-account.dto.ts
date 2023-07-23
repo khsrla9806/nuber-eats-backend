050d58a04f1b68aecd508aedaada71d8a3e320fc
khsrla9806 <kimhunsope@naver.com>
2023-07-23 17:22:55 +0900

    refactor: 공통적으로 사용되는 OutputDto를 분리하여 CreateAccountOutput 리펙토링
    - 이때 분리된 MutationOutput과 기존의 CreateAccountOutput 둘다 @ObjectType이 붙어야 한다.

--- src/users/dtos/create-account.dto.ts
+++ src/users/dtos/create-account.dto.ts
@@ -1,6 +1,7 @@
 import { Field, InputType, ObjectType, PickType } from "@nestjs/graphql";
 import { User } from "../entities/user.entity";
 import { type } from "os";
+import { MutationOutput } from "src/common/dtos/output.dto";
 
 /* PickType은 원하는 필드들만 골라서 가져올 수 있다. */
 @InputType()
@@ -8,10 +9,4 @@ export class CreateAccountInput extends PickType(User, ['email', 'password', 'ro
 }
 
 @ObjectType()
-export class CreateAccountOutput {
-    @Field(type => String, { nullable: true })
-    error?: string;
-
-    @Field(type => Boolean)
-    ok: boolean;
-}
+export class CreateAccountOutput extends MutationOutput {}
