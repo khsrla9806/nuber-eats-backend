4b9e21f0de5e60c8a2053955dc7c335af98cd9a3
khsrla9806 <kimhunsope@naver.com>
2023-07-23 17:23:52 +0900

    feat: User Entity에 Validation 추가
    - @IsEmail
    - @IsEnum(Enum의 Type)

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,6 +3,7 @@ import { CoreEntity } from "src/common/entities/core.entity";
 import { BeforeInsert, Column, Entity } from "typeorm";
 import * as bcrypt from "bcrypt";
 import { InternalServerErrorException } from "@nestjs/common";
+import { IsEmail, IsEnum } from "class-validator";
 
 enum UserRole {
     Client,
@@ -19,6 +20,7 @@ registerEnumType(UserRole, { name: 'UserRole' });
 export class User extends CoreEntity {
 
     @Field(type => String)
+    @IsEmail()
     @Column()
     email: string;
 
@@ -27,6 +29,7 @@ export class User extends CoreEntity {
     password: string;
 
     @Field(type => UserRole) // GraphQL에 등록한 enum을 @Field의 type으로 지정
+    @IsEnum(UserRole)
     @Column({type: 'enum', enum: UserRole})
     role: UserRole;
 
