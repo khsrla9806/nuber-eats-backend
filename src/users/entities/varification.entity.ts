0066e97dd9fe83377d5e8b2aef3241900ad89924
khsrla9806 <kimhunsope@naver.com>
2023-07-27 22:25:59 +0900

    feat: User 삭제 시 관련된 Verification도 함께 삭제되도록 Cascade 속성 추가

--- src/users/entities/varification.entity.ts
+++ src/users/entities/varification.entity.ts
@@ -13,7 +13,7 @@ export class Verification extends CoreEntity {
     @Field(type => String)
     code: string;
 
-    @OneToOne(type => User)
+    @OneToOne(type => User, { onDelete: "CASCADE"}) // 관련된 User가 삭제되면 Verification도 같이 삭제
     @JoinColumn()
     user: User;
 
